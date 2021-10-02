23557b6eef1a3668f6c4bf1016a3b050fdb338e2
user <user@users-MacBook-Pro.local>
2021-10-01 20:06:37 -0700

    still working on style of directory site to matach mockups. Working on creating modal for when element is clicked

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
   const employeeDir = document.getElementById('directory');
+  const body = document.querySelector('body')
   const employeesURL = 'https://randomuser.me/api/?format=JSON?page=3&results=12&seed=abc'
 
   function generateDir(data) {
     data.results.map(person => {
       const section = document.createElement('section');
         section.innerHTML = `
-          <img src='${person.picture.thumbnail}'>
-          <div class='employee-info'>
-            <h2>${person.name.first} ${person.name.last}</h2>
-            <p>${person.email}</p>
-            <p>${person.location.city}</p>
+          <div class='employee-container'>
+            <img src='${person.picture.thumbnail}'>
+            <div class='employee-info'>
+              <h2>${person.name.first} ${person.name.last}</h2>
+              <p>${person.email}</p>
+              <p>${person.location.city}</p>
+            </div>
           </div>`;
       employeeDir.appendChild(section)
     })
-    console.log()
+  }
+
+  function createModal() {
+    const div = document.createElement('div');
+    body.insertAdjacentElement('afterbegin', div)
+    div.className = 'modal-background'
+    div.innerHTML = `
+        <div class='modal-container'>
+          <div class='modal-box'>
+            <h1>Hello World</h1>
+          </div>
+        </div>
+    `
   }
 
   fetch(employeesURL)
     .then(data => data.json())
     .then(generateDir)
     .catch(err => console.log(err))
+
+  body.addEventListener('click', (e) => {
+   
+      if(e.target.className === 'employee-info')
+      console.log('hello world')
+  })
 })
